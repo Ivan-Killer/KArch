c26d057a103b6d324c612fbcd17daac9107fa15c
Ivan Stefanov <register_1994@abv.bg>
2017-05-15 21:48:42 +0300

    Add files via upload

---
+++ Test 3/ESP8266_karch_var_declaration.h
@@ -0,0 +1,20 @@
+char ssid[] = "elsys-cab37";
+char pass[] = "elsys-bg.org";
+
+unsigned int UDPPort_listen = 20200;      // local port to listen on
+unsigned int UDPPort_server = 20200;      // RxD port of Home Automation Server(HAS)
+char packetBuffer[255]; //buffer to hold incoming packet
+//char replyBuffer[] = "acknowledged";       // a string to send back
+
+IPAddress HAS_ip(192, 168, 37, 156); // IP address of HAS
+IPAddress remote_ip(192, 168, 37, 156); // initial IP address UDP reply sender
+
+char Hello[] = "###H;###";    // Hello cmd
+char IDcmd[] = "###I;kp;###";    // ID cmd
+char Var_dec[] = "###D;light_intensity@R;state@RW;power@RW;###"; // Var declaration
+//char Var_set[] = "###W;small_lamp.light_intensity=1;###"; // Can't write to HAS (not ready)
+char Read_cmd[] = "###R;small_lamp.state;small_lamp.power;###";
+
+char power[15];
+char state;
+byte dim_val = 0;
